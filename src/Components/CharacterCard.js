592a46c78cf1655da011c801917e1472fd76ed00
rskiver89 <108497515+rskiver89@users.noreply.github.com>
2023-05-05 18:27:00 -0400

    update character card

--- src/Components/CharacterCard.js
+++ src/Components/CharacterCard.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import "../Styles/Characters.css"
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../Styles/Characters.css";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
+function CharacterCard({ character }) {
+  const cardVariants = {
+    hover: { scale: 1.1 },
+    tap: { scale: 0.95 },
+  };
 
-function CharacterCard({character}) {
-    return ( 
-        <div className="character-card">
+  return (
+    <motion.div
+      className="character-card"
+      whileHover="hover"
+      whileTap="tap"
+      variants={cardVariants}
+    >
+    <Link to={`/details/${character.id}`}>
         <img src={character.image} />
-        <p>{character.name}</p>
-        <Link to={`/details/${character.id}`}><button>See Details</button></Link>
-        
-        </div>
-     );
+    </Link>
+      <p>{character.name}</p>
+    </motion.div>
+  );
 }
 
 export default CharacterCard;
